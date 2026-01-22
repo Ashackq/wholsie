afbae5c15bdf0ae8e2869ac2c4231efc880db40e
Ashackq <akashpatelyo2@gmail.com>
2026-01-22 19:14:37 +0530

    [update] server out

--- server/src/config/db.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import { env } from "./env.js";
-
-export async function connectToDatabase() {
-    mongoose.set("strictQuery", true);
-    await mongoose.connect(env.MONGODB_URI);
-}
